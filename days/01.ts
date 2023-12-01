59513ff0a9865cd661ac031fa9cc15d203be08d5
Gwolpi <gwolpi@users.noreply.github.com>
2023-12-01 13:54:45 +0100

    Day 01

--- days/01.ts
+++ days/01.ts
@@ -3,10 +3,8 @@ import '../extension-methods.ts';
 export const p1 = (input: string): number =>
   input.splitRows().reduce((acc, x) => {
     const nums = x.replace(/\D/g, '');
-    const calibration = +`${nums.at(0)}${nums.at(-1)}`;
-    return acc + calibration;
+    return acc + Number(`${nums.at(0)}${nums.at(-1)}`);
   }, 0);
-
 
 export const p2 = (input: string): number => {
   const textNums: Record<string, number> = {one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9};
@@ -14,7 +12,6 @@ export const p2 = (input: string): number => {
   return input.splitRows().reduce((acc, x) => {
     const [, a] = x.match(regex) ?? [];
     const [, b] = x.match(`.*${regex}`) ?? [];
-    const calibration = +`${textNums[a] ?? a}${textNums[b] ?? b}`
-    return acc + calibration;
+    return acc + Number(`${textNums[a] ?? a}${textNums[b] ?? b}`);
   }, 0);
 }
