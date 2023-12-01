5baeaea62fadc92c494753ff5309c091832fc8af
Gwolpi <gwolpi@users.noreply.github.com>
2023-12-01 12:24:10 +0100

    Day 01

--- days/01.ts
+++ days/01.ts
@@ -10,10 +10,10 @@ export const p1 = (input: string): number =>
 
 export const p2 = (input: string): number => {
   const textNums: Record<string, number> = {one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9};
-  const textNumsKeys = `(\\d|${Object.keys(textNums).join("|")})`
+  const regex = `(\\d|${Object.keys(textNums).join("|")})`
   return input.splitRows().reduce((acc, x) => {
-    const [, a] = x.match(textNumsKeys) ?? [];
-    const [, b] = x.match(`.*${textNumsKeys}`) ?? [];
+    const [, a] = x.match(regex) ?? [];
+    const [, b] = x.match(`.*${regex}`) ?? [];
     const calibration = +`${textNums[a] ?? a}${textNums[b] ?? b}`
     return acc + calibration;
   }, 0);
