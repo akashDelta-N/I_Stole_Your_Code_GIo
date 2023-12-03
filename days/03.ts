e6402abb797cec857a2585d18cc4d1f1ff62918e
Gwolpi <gwolpi@users.noreply.github.com>
2023-12-03 15:55:02 +0100

    Day 03

--- days/03.ts
+++ days/03.ts
@@ -20,11 +20,11 @@ export const p1 = (input: string): number => {
   const matrix = input.splitRows();
   const parts: Array<number> = [];
   matrix.forEach((row, y) => {
-    row.matchMap(/\d+/g, ({0: number, index}) => {
-      for (let i = index ?? 0, x = i; x < i + number.length; x++) {
+    row.matchMap(/\d+/g, ({0: part, index}) => {
+      for (let i = index ?? 0, x = i; x < i + part.length; x++) {
         const [hasPart] = checkNeighbours(matrix, x, y, /^((?![.\d\s]).)*$/);
         if(!hasPart) continue;
-        parts.push(Number(number));
+        parts.push(Number(part));
         break;
       }
     })
@@ -36,11 +36,11 @@ export const p2 = (input: string): number => {
   const matrix = input.splitRows();
   const gears: Record<string, Array<number>> = {};
   matrix.forEach((row, y) => {
-    row.matchMap(/\d+/g, ({0: number, index}) => {
-      for (let i = index ?? 0, x = i; x < i + number.length; x++) {
+    row.matchMap(/\d+/g, ({0: part, index}) => {
+      for (let i = index ?? 0, x = i; x < i + part.length; x++) {
         const [hasPart, coordinates] = checkNeighbours(matrix, x, y, /^\*$/);
         if(!hasPart) continue;
-        (gears[coordinates] ??= []).push(Number(number));
+        (gears[coordinates] ??= []).push(Number(part));
         break;
       }
     })
