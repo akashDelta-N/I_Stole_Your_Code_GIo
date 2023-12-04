f349abc195cac7980c82d09d929f9c0758d7628a
Gwolpi <gwolpi@users.noreply.github.com>
2023-12-04 12:17:03 +0100

    Day 04

--- days/04.ts
+++ days/04.ts
@@ -1,22 +1,21 @@
 import '../extension-methods.ts';
 
-const processInput = (input: string) => input.matchMap(/^Card +\d+:\s+((?:\d+\s+)+)\|((?:\s+\d+)+)$/gm, ([, win, num]) => {
-  const parseNums = (nums: string) => nums.trim().split(/\s+/g).map(Number);
-  return { winning: parseNums(win), numbers: parseNums(num), cardCount: 1 };
-});
+type Card = { winning: number[], numbers: number[], cardCount: number };
 
-export const p1 = (input: string): number => processInput(input)
-  .map(({winning, numbers}) => {
-    const matches = numbers.count(num => winning.includes(num));
-    return matches ? 2 ** (matches - 1) : 0;
-  })
-  .reduce((acc, x) => acc + x, 0);
+const processInput = (input: string, map: (card: Card, index: number, cards: Card[]) => number) =>
+  input.matchMap(/^.+:\s+([\d\s]+)\s\|\s([\s\d]+)$/gm, ([, win, num]) => {
+    const parseNums = (nums: string) => nums.split(/\s+/g).map(Number);
+    return { winning: parseNums(win), numbers: parseNums(num), cardCount: 1 };
+  }).map(map).reduce((acc, x) => acc + x, 0);
 
-export const p2 = (input: string): number => processInput(input)
-  .map(({winning, numbers, cardCount}, index, cards) => {
-    const matches = numbers.count(num => winning.includes(num));
-    for (let i = 1; i <= matches && index + i < cards.length; i++)
-      cards[index + i].cardCount += cardCount;
-    return cardCount;
-  })
-  .reduce((acc, x) => acc + x, 0);
+export const p1 = (input: string): number => processInput(input, ({winning, numbers}) => {
+  const matches = numbers.count(num => winning.includes(num));
+  return matches ? 2 ** (matches - 1) : 0;
+})
+
+export const p2 = (input: string): number => processInput(input, ({winning, numbers, cardCount}, index, cards) => {
+  const matches = numbers.count(num => winning.includes(num));
+  for (let i = 1; i <= matches && index + i < cards.length; i++)
+    cards[index + i].cardCount += cardCount;
+  return cardCount;
+})
