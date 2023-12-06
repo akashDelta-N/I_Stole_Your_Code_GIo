09cd8dc50aade64f252a21904735fff99ad9af87
Gwolpi <gwolpi@users.noreply.github.com>
2023-12-06 20:40:27 +0100

    Day 06

--- days/06.ts
+++ days/06.ts
@@ -1,13 +1,14 @@
 import '../extension-methods.ts';
 
-export const p1 = (input: string): number => {
-  const [times, records] = input.splitRows().map((row) => row.matchMap(/\d+/g, Number));
-  return times.reduce((acc, time, i) => {
-    let timesBeaten = 0;
-    for (let hold = 0, record = records[i]; hold < time; ++hold)
-      timesBeaten += Number(hold * (time - hold) > record);
-    return acc * timesBeaten;
-  }, 1);
+const calculateTimesBeaten = (time: number, distance: number): number => {
+  const d = Math.sqrt(time ** 2 - 4 * (distance + 1));
+  const [min, max] = [(-time + d) / -2, (-time - d) / -2].sortNums();
+  return Math.floor(max) - Math.ceil(min) + 1;
 }
 
-export const p2 = (input: string): number => p1(input.replace(/ /g, ''))
+export const p1 = (input: string): number => {
+  const [times, distances] = input.splitRows().map((row) => row.matchMap(/\d+/g, Number));
+  return times.map((time, i) => calculateTimesBeaten(time, distances[i])).prod();
+}
+
+export const p2 = (input: string): number => p1(input.replace(/ +/g, ''))
