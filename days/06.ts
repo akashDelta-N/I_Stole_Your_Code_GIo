eb2fac5c9901cdd4527749494a5aa866bb7b2dc6
Gwolpi <gwolpi@users.noreply.github.com>
2023-12-06 12:11:41 +0100

    Day 06

--- days/06.ts
+++ days/06.ts
@@ -3,10 +3,9 @@ import '../extension-methods.ts';
 const processInput = (input: string) => {
   const [times, records] = input.splitRows().map((row) => row.matchMap(/\d+/g, Number));
   return times.reduce((acc, time, i) => {
-    const record = records[i];
     let timesBeaten = 0;
     for (let hold = 0; hold < time; ++hold)
-      timesBeaten += Number(hold * (time - hold) > record);
+      timesBeaten += Number(hold * (time - hold) > records[i]);
     return acc * timesBeaten;
   }, 1);
 }
