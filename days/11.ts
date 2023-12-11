4e134dcda2b97c98452a03f5325e26e0d1a1f070
Wolpert <4539521+gwolpi@users.noreply.github.com>
2023-12-11 23:24:52 +0100

    Day 11

--- days/11.ts
+++ days/11.ts
@@ -4,8 +4,8 @@ export const p1 = (input: string, factor = 2): number => {
 	const space: string[][] = input
 		.splitRows()
 		.map((line) => [...line])
-		.map((row) => row.every((cell) => cell !== '#') ? row.map(() => '$') : row)
-		.map((row, _, space) => row.map((cell, x) => space.every((r) => r[x] !== '#') ? '$' : cell));
+		.map((row) => row.every((cell) => cell !== '#') ? row.map(() => 'X') : row)
+		.map((row, _, space) => row.map((cell, x) => space.every((r) => r[x] !== '#') ? 'X' : cell));
 	return space
 		.reduce((acc, row, y) => {
 			const entries = row.map((cell, x) => cell === '#' ? [x, y] : []).filter(Boolean);
@@ -15,10 +15,10 @@ export const p1 = (input: string, factor = 2): number => {
 		.reduce((acc, [x1, y1], index, galaxies) => {
 			galaxies.slice(0, index).forEach(([x2, y2]) => {
 				for (let x = Math.min(x1, x2); x < Math.max(x1, x2); x++) {
-					acc += space[y1][x] === '$' ? factor : 1;
+					acc += space[y1][x] === 'X' ? factor : 1;
 				}
 				for (let y = Math.min(y1, y2); y < Math.max(y1, y2); y++) {
-					acc += space[y][x1] === '$' ? factor : 1;
+					acc += space[y][x1] === 'X' ? factor : 1;
 				}
 			});
 			return acc;
