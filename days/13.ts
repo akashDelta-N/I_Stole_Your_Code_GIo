151a77612ac1965251d9e9fca85b2818ce9892eb
Wolpert <4539521+gwolpi@users.noreply.github.com>
2023-12-13 20:07:12 +0100

    Day 13

--- days/13.ts
+++ days/13.ts
@@ -1,38 +1,43 @@
 import '../extension-methods.ts';
 
+type SurroundingPatterns = { next: string[][]; prev: string[][] };
+
 const processInput = (input: string, lineOfReflection: (pattern: string[][]) => number | undefined) =>
 	input.splitRows(2)
 		.map((pattern) => pattern.splitRows().map((row) => [...row]))
-		.reduce((sum, pattern) => {
+		.reduce((acc, pattern) => {
 			const result = lineOfReflection(pattern);
-			if (result) return sum + 100 * result;
+			if (result) return acc + 100 * result;
 			pattern = pattern[0].map((_, i) => pattern.map((row) => row[i]));
-			return sum + lineOfReflection(pattern)!;
+			return acc + lineOfReflection(pattern)!;
 		}, 0);
 
-const lineFound = (prev: string[][], next: string[][]) =>
-  prev.every((row, y) => row.every((cell, x) => cell === next[y][x]));
+const getSurroundingPatterns = (pattern: string[][], index: number): SurroundingPatterns => {
+	const next = pattern.slice(index, index * 2).reverse();
+	const prev = pattern.slice(0, index).slice(-next.length);
+	return { next, prev };
+};
+
+const lineFound = ({ next, prev }: SurroundingPatterns) =>
+	prev.every((row, y) => row.every((cell, x) => cell === next[y][x]));
 
 export const p1 = (input: string): number =>
 	processInput(input, (pattern) => {
 		for (let i = 1; i < pattern.length; i++) {
-			const next = pattern.slice(i, 2 * i).reverse();
-			const prev = pattern.slice(0, i).slice(-next.length);
-			if (lineFound(prev, next)) return i;
+			if (lineFound(getSurroundingPatterns(pattern, i))) return i;
 		}
 	});
 
 export const p2 = (input: string): number =>
 	processInput(input, (pattern) => {
 		for (let i = 1; i < pattern.length; i++) {
-			const next = pattern.slice(i, i * 2).reverse();
-			const prev = pattern.slice(0, i).slice(-next.length);
-			const fixSmudge = (y: number, x: number) => prev[y][x] = { ['.']: '#', ['#']: '.' }[prev[y][x]]!;
+			const { next, prev } = getSurroundingPatterns(pattern, i);
+			const fixSmudge = (x: number, y: number) => prev[y][x] = { ['.']: '#', ['#']: '.' }[prev[y][x]]!;
 			for (const prevY of prev.keys()) {
 				for (const prevX of prev[prevY].keys()) {
-					fixSmudge(prevY, prevX);
-					if (lineFound(prev, next)) return i;
-					fixSmudge(prevY, prevX);
+					fixSmudge(prevX, prevY);
+					if (lineFound({ next, prev })) return i;
+					fixSmudge(prevX, prevY);
 				}
 			}
 		}
