78edd5cf76104405e03a9dd1cb841df27876c410
Wolpert <4539521+gwolpi@users.noreply.github.com>
2023-12-13 23:42:36 +0100

    Day 12

--- days/12.ts
+++ days/12.ts
@@ -10,39 +10,36 @@ const processInput = (input: string) =>
 		groups: groupsStr.split(',').map(Number),
 	}));
 
-const find: FindArrangementsFn = memoizeFunction(findArrangements);
-
 // https://en.wikipedia.org/wiki/Memoization
-function memoizeFunction(fn: FindArrangementsFn): FindArrangementsFn {
-	const cache = new Map();
-	return (...args: [Springs, number[], number]) => {
-		const key = JSON.stringify(args);
-		if (cache.has(key)) return cache.get(key);
-		const result = fn(...args);
-		cache.set(key, result);
-		return result;
-	};
+const memoizeFunction = (fn: FindArrangementsFn): FindArrangementsFn => {
+  const cache = new Map();
+  return (...args: [Springs, number[], number]) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) return cache.get(key);
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
 }
 
-function findArrangements(springs: Springs, groups: number[], i: number): number {
+const find = memoizeFunction((springs: Springs, groups: number[], i: number): number => {
 	const [spring, ...remainingSprings] = springs;
 	const [group, ...remainingGroups] = groups;
 	const running = Boolean(~i);
 
 	if (!spring) return Number(!groups.length && !running || groups.length === 1 && group === i);
-	const groupSizeReached = i === group && running;
 	const operational = spring === status.Operational;
 	const damaged = spring === status.Damaged;
 	const unknown = spring === status.Unknown;
 
 	let arrangements = 0;
-	arrangements += groupSizeReached && operational ? find(remainingSprings, remainingGroups, -1) : 0;
-	arrangements += groupSizeReached && unknown ? find(remainingSprings, remainingGroups, -1) : 0;
-	arrangements += (unknown || damaged) && running ? find(remainingSprings, groups, i + 1) : 0;
 	arrangements += (unknown || damaged) && !running ? find(remainingSprings, groups, 1) : 0;
+	arrangements += (unknown || damaged) && running ? find(remainingSprings, groups, i + 1) : 0;
 	arrangements += (unknown || operational) && !running ? find(remainingSprings, groups, -1) : 0;
+	arrangements += (unknown || operational) && running && i === group ? find(remainingSprings, remainingGroups, -1) : 0;
 	return arrangements;
-}
+});
+
 
 export const p1 = (input: string): number =>
 	processInput(input).map(({ springs, groups }) => find(springs, groups, -1)).sum();
