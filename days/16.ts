654d7732c9661182cab0debf1709575abc03868c
Wolpert <4539521+gwolpi@users.noreply.github.com>
2023-12-17 00:31:25 +0100

    Day 16

--- days/16.ts
+++ days/16.ts
@@ -1,6 +1,6 @@
 import { Direction, Matrix } from '../extension-methods.ts';
 
-type Beam = { direction: Direction; xy: string };
+type Beam = { direction: Direction; location: string };
 
 const processBeam = (matrix: Matrix, startBeam: Beam) => {
 	const beams: Beam[] = [startBeam];
@@ -9,13 +9,13 @@ const processBeam = (matrix: Matrix, startBeam: Beam) => {
 	const mirrorSlantedRight: Record<string, Direction> = { '↑': '→', '↓': '←', '←': '↓', '→': '↑' };
 	const mirrorSlantedLeft: Record<string, Direction> = { '↑': '←', '↓': '→', '←': '↑', '→': '↓' };
 	while (beams.length) {
-		const { direction, xy: location } = beams.shift()!;
-		const neighbours = Object.entries(matrix.neighboursByString(location, direction));
+		const { direction, location: currentLocation } = beams.shift()!;
+		const neighbours = Object.entries(matrix.neighboursByString(currentLocation, direction));
 		if (!neighbours.length) continue;
-		const [xy, tileValue] = neighbours[0];
-		if (!tileValue || visitedFromDirection.has(xy + direction)) continue;
-		visited.add(xy);
-		visitedFromDirection.add(xy + direction);
+		const [location, tileValue] = neighbours[0];
+		if (!tileValue || visitedFromDirection.has(location + direction)) continue;
+		visited.add(location);
+		visitedFromDirection.add(location + direction);
 		const findNextDirections: Record<string, () => Direction[]> = {
 			'.': () => [direction],
 			'|': () => '↑↓'.includes(direction) ? [direction] : ['↑', '↓'],
@@ -23,26 +23,26 @@ const processBeam = (matrix: Matrix, startBeam: Beam) => {
 			'/': () => [mirrorSlantedRight[direction]],
 			'\\': () => [mirrorSlantedLeft[direction]],
 		};
-		findNextDirections[tileValue]().forEach((direction) => beams.push({ direction, xy }));
+		findNextDirections[tileValue]().forEach((direction) => beams.push({ direction, location }));
 	}
 	return visited.size;
 };
 
 export const p1 = (input: string): number => {
 	const matrix = new Matrix(input.splitRows().map((x) => [...x]));
-	return processBeam(matrix, { direction: '→', xy: 'x-1y0' });
+	return processBeam(matrix, { direction: '→', location: 'x-1y0' });
 };
 
 export const p2 = (input: string): number => {
 	const matrix = new Matrix(input.splitRows().map((x) => [...x]));
 	const startBeams: Beam[] = [];
 	for (let x = 0; x < matrix.width; x++) {
-		startBeams.push({ direction: '↓', xy: `x${x}y-1` });
-		startBeams.push({ direction: '↑', xy: `x${x}y${matrix.length}` });
+		startBeams.push({ direction: '↓', location: `x${x}y-1` });
+		startBeams.push({ direction: '↑', location: `x${x}y${matrix.length}` });
 	}
 	for (let y = 0; y < matrix.length; y++) {
-		startBeams.push({ direction: '→', xy: `x-1y${y}` });
-		startBeams.push({ direction: '←', xy: `x${matrix.width}y${y}` });
+		startBeams.push({ direction: '→', location: `x-1y${y}` });
+		startBeams.push({ direction: '←', location: `x${matrix.width}y${y}` });
 	}
 	return Math.max(...startBeams.map((startBeam) => processBeam(matrix, startBeam)));
 };
