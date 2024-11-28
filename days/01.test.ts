cf4ff18f01384e07d634900f70d0242372f12441
DELTA-N\akasha <akasha@delta-n.nl>
2024-11-28 08:45:46 +0100

    INIT Removed Gio's solutions

--- days/01.test.ts
+++ days/01.test.ts
@@ -1,17 +1,17 @@
-import { assertEquals } from 'std/assert/assert_equals.ts';
+import {assertEquals} from "std/assert/assert_equals.ts";
 import '../extension-methods.ts';
-import { p1, p2 } from './01.ts';
+import {p1, p2} from './01.ts';
+
+const input = '';
 
 Deno.test('it should run the first part of day 01 correctly', () => {
-	const result = p1('1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet');
-	const expected = 142;
-	assertEquals(result, expected);
+  const result = p1(input);
+  const expected = 0;
+  assertEquals(result, expected);
 });
 
 Deno.test('it should run the second part of day 01 correctly', () => {
-	const result = p2(
-		'two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen',
-	);
-	const expected = 281;
-	assertEquals(result, expected);
+  const result = p2(input);
+  const expected = 0;
+  assertEquals(result, expected);
 });
--- days/01.ts
+++ days/01.ts
@@ -1,26 +1,11 @@
 import '../extension-methods.ts';
 
-export const p1 = (input: string): number =>
-	input.splitRows().reduce((acc, x) => {
-		const nums = x.replace(/\D/g, '');
-		return acc + Number(`${nums.at(0)}${nums.at(-1)}`);
-	}, 0);
+export const p1 = (input: string): number => {
+  const lines = input.splitRows().map(Number);
+  return lines.length;
+}
 
 export const p2 = (input: string): number => {
-	const digits: Record<string, number> = {
-		one: 1,
-		two: 2,
-		three: 3,
-		four: 4,
-		five: 5,
-		six: 6,
-		seven: 7,
-		eight: 8,
-		nine: 9,
-	};
-	const keys = `(\\d|${Object.keys(digits).join('|')})`;
-	const regex = RegExp(`${keys}(?:.*${keys})?`, 'gm');
-	return input
-		.matchMap(regex, ([, a, b]) => `${digits[a] ?? a}${digits[b ?? a] ?? b ?? a}`)
-		.reduce((acc, x) => acc + Number(x), 0);
-};
+  const lines = input.splitRows().map(Number);
+  return lines.length;
+}
