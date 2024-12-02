15e5c3e070c75a13487f928c15f05f4747694f77
DELTA-N\akasha <akasha@delta-n.nl>
2024-12-02 17:35:52 +0100

    Improved Day 1

--- days/01.ts
+++ days/01.ts
@@ -1,27 +1,14 @@
 import '../extension-methods.ts';
 
 export const p1 = (input: string): number => {
-  const lines = input.splitRows().map(String);
-  let differences:number[] = [];
-  let leftNumbers:number[] = [];
-  let rightNumbers:number[] = [];
-
-  lines.forEach((line, index) => {
-    const splitLine = line.split('  ').map(Number);
-    const left = splitLine[0];
-    const right = splitLine[1];
-    leftNumbers.push(left);
-    rightNumbers.push(right);
+  const lines = input.splitRows().map(line => {
+    const [left,right] = line.split('  ').map(Number);
+    return {left, right};
   });
 
-  leftNumbers = leftNumbers.sort((a, b) => a - b);
-  rightNumbers = rightNumbers.sort((a, b) => a - b);
-
-  for (let i = 0; i < leftNumbers.length; i++) {
-    const left = leftNumbers[i];
-    const right = rightNumbers[i];
-    differences.push(Math.abs(left - right));
-  }
+  const leftNumbers = lines.map(line => line.left).sort((a, b) => a - b);
+  const rightNumbers = lines.map(line => line.right).sort((a, b) => a - b);
+  let differences:number[] = leftNumbers.map((left, index) => Math.abs(left - rightNumbers[index]));
 
   const total = differences.reduce((acc, curr) => acc + curr, 0);
   return total;
@@ -37,7 +24,6 @@ export const p2 = (input: string): number => {
   const rightNumbers = lines.map(line => line.right);
 
   const simularityScore = leftNumbers.map((left, index) => {
-    
     return left * rightNumbers.filter(right => right === left).length;
   });
   return simularityScore.reduce((acc, curr) => acc + curr, 0);
