c9d7b943c347ae5f9be14844b3fa14d732d056b9
DELTA-N\akasha <akasha@delta-n.nl>
2024-12-02 09:59:49 +0100

    Day 1, no optimization

--- days/01.test.ts
+++ days/01.test.ts
@@ -2,16 +2,21 @@ import {assertEquals} from "std/assert/assert_equals.ts";
 import '../extension-methods.ts';
 import {p1, p2} from './01.ts';
 
-const input = '';
+const input = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
 
 Deno.test('it should run the first part of day 01 correctly', () => {
   const result = p1(input);
-  const expected = 0;
+  const expected = 11;
   assertEquals(result, expected);
 });
 
 Deno.test('it should run the second part of day 01 correctly', () => {
   const result = p2(input);
-  const expected = 0;
+  const expected = 31;
   assertEquals(result, expected);
 });
--- days/01.ts
+++ days/01.ts
@@ -1,11 +1,44 @@
 import '../extension-methods.ts';
 
 export const p1 = (input: string): number => {
-  const lines = input.splitRows().map(Number);
-  return lines.length;
+  const lines = input.splitRows().map(String);
+  let differences:number[] = [];
+  let leftNumbers:number[] = [];
+  let rightNumbers:number[] = [];
+
+  lines.forEach((line, index) => {
+    const splitLine = line.split('  ').map(Number);
+    const left = splitLine[0];
+    const right = splitLine[1];
+    leftNumbers.push(left);
+    rightNumbers.push(right);
+  });
+
+  leftNumbers = leftNumbers.sort((a, b) => a - b);
+  rightNumbers = rightNumbers.sort((a, b) => a - b);
+
+  for (let i = 0; i < leftNumbers.length; i++) {
+    const left = leftNumbers[i];
+    const right = rightNumbers[i];
+    differences.push(Math.abs(left - right));
+  }
+
+  const total = differences.reduce((acc, curr) => acc + curr, 0);
+  return total;
 }
 
 export const p2 = (input: string): number => {
-  const lines = input.splitRows().map(Number);
-  return lines.length;
+  const lines = input.splitRows().map(line => {
+    const [left, right] = line.split('  ').map(Number);
+    return {left, right};
+  });
+  
+  const leftNumbers = lines.map(line => line.left);
+  const rightNumbers = lines.map(line => line.right);
+
+  const simularityScore = leftNumbers.map((left, index) => {
+    
+    return left * rightNumbers.filter(right => right === left).length;
+  });
+  return simularityScore.reduce((acc, curr) => acc + curr, 0);
 }
